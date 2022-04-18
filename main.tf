e24c18dac7d00089c54f173e0a7fd9541b6549b9
Marcus Lenngren <marcus.lenngren@getpfc.com>
2022-04-18 11:48:49 +0200

    Remove commented out key

--- main.tf
+++ main.tf
@@ -78,7 +78,6 @@ resource "aws_security_group" "main_sg" {
 
 resource "aws_key_pair" "main_auth" {
   key_name   = "mainkey"
-  #public_key = file("~/.ssh/mainawskey.pub")
   public_key = var.MAINAWSKEYPUB
 }
 
